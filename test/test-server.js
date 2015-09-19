31842ba1445feae93455704b6a04a531f69d392d
Stu <stuart@blueprintinteractive.com>
2015-09-19 18:45:42 -0400

    start work on PUT test, error is present

--- test/test-server.js
+++ test/test-server.js
@@ -54,6 +54,30 @@ describe('Shopping List', function() {
                 done();
             });
     });
-    it('should edit an item on put');
+    it('should edit an item on put', function(done) {
+        chai.request(app)
+            .put('/items/1')
+            .send({'name': 'Bob'})
+            .end(function(err, res) {
+                should.equal(err, null);
+                res.should.have.status(201);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.should.have.property('name');
+                res.body.should.have.property('id');
+                res.body.name.should.be.a('string');
+                res.body.id.should.be.a('number');
+                res.body.name.should.equal('Bob');
+                storage.items.should.be.a('array');
+                storage.items.should.have.length(4);
+                storage.items[1].should.be.a('object');
+                storage.items[1].should.have.property('id');
+                storage.items[1].should.have.property('name');
+                storage.items[1].id.should.be.a('number');
+                storage.items[1].name.should.be.a('string');
+                storage.items[1].name.should.equal('Bob');
+                done();
+            });
+    });
     it('should delete an item on delete');
 });
