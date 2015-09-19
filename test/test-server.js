71be854dbe4f75d4d8b5ff4a128d3120d24c625f
Stu <stuart@blueprintinteractive.com>
2015-09-19 18:35:52 -0400

    add second test for POST

--- test/test-server.js
+++ test/test-server.js
@@ -29,7 +29,31 @@ describe('Shopping List', function() {
                 done();
             });
     });
-    it('should add an item on post');
+    it('should add an item on POST', function(done) {
+        chai.request(app)
+            .post('/items')
+            .send({'name': 'Kale'})
+            .end(function(err, res) {
+                should.equal(err, null);
+                res.should.have.status(201);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.should.have.property('name');
+                res.body.should.have.property('id');
+                res.body.name.should.be.a('string');
+                res.body.id.should.be.a('number');
+                res.body.name.should.equal('Kale');
+                storage.items.should.be.a('array');
+                storage.items.should.have.length(4);
+                storage.items[3].should.be.a('object');
+                storage.items[3].should.have.property('id');
+                storage.items[3].should.have.property('name');
+                storage.items[3].id.should.be.a('number');
+                storage.items[3].name.should.be.a('string');
+                storage.items[3].name.should.equal('Kale');
+                done();
+            });
+    });
     it('should edit an item on put');
     it('should delete an item on delete');
 });
